78dc755e954fab72a17c0ec0b535fc833f61718c
Yi Han <hanystudy@gmail.com>
2017-09-04 00:07:58 +0800

    [hy] add more test case

---
+++ spec/plots/navigate_dogs_page.rb
@@ -0,0 +1,7 @@
+SnowyOwl::Plots.write 'navigate dogs page' do
+  expect(page.has_css?('div#Header')).to eq true
+  expect(page.has_css?('div#Content')).to eq true
+  expect(page.has_css?('div#Footer')).to eq true
+  page.find('div#Logo a[href="/jpetstore/actions/Catalog.action"]').click
+  page.find('div#Sidebar a[href*="categoryId=DOGS"]').click
+end
--- spec/plots/navigate_shopping_cart.rb
+++ spec/plots/navigate_shopping_cart.rb
@@ -1,5 +1,6 @@
-SnowyOwl::Plots.write 'navigate home page' do
+SnowyOwl::Plots.write 'navigate shopping cart' do
   expect(page.has_css?('div#Header')).to eq true
   expect(page.has_css?('div#Content')).to eq true
   expect(page.has_css?('div#Footer')).to eq true
+  page.find('div#MenuContent a[href="/jpetstore/actions/Cart.action?viewCart="]').click
 end
